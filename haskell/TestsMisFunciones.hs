5b98ba3e86d4e844523077ec87f5f99a57a7e7dc
rjavier97 <rjavier.suarez@hotmail.com>
2023-09-20 14:01:03 -0300

    bug fixed testHUnit

---
+++ haskell/TestsMisFunciones.hs
@@ -0,0 +1,12 @@
+module TestsMisFunciones where
+import Test.HUnit 
+import MisFunciones 
+
+-- Casos de test 
+run = runTestTT tests
+
+tests = test [ 
+    " Caso base 1: fib 0" ~: fib 0 ~?= 0, 
+    " Caso base 2: fib 1" ~: fib 1 ~?= 1,
+    " Caso recursivo 1: fib 2" ~: fib 2 ~?= 1 
+ ]
