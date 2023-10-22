78e73de687e91674054794552200a75f7122b0c3
Ricardo Javier Suarez <rjsuarez@dc.uba.ar>
2023-10-22 00:05:49 -0300

    update guia7

--- python/guia7.py
+++ python/guia7.py
@@ -25,10 +25,17 @@ def pertenece1(s: [int], e: int)->bool:
 def pertenece2(s:[int], e:int)->bool:
     pertenece: bool = False
     longitud: int = len(s)
-    for i in range(0, longitud, 1):
+    for i in range(0, longitud, 1):   ## Como star=0 , y step:1 son valor por default , se puede poner in range(longitud) nomas
         if (s[i] == e) :
             pertenece = True
     return pertenece    
+
+def pertenece3(s: [int], e: int)->bool:
+    pertenece: bool = False
+    for elemento in s :
+        if elemento == e : 
+            pertenece = True 
+    return pertenece 
 
 def perteneceGenerico(s:[any], e:any)->bool:
     pertenece: bool = False
@@ -38,8 +45,61 @@ def perteneceGenerico(s:[any], e:any)->bool:
             pertenece = True
     return pertenece    
 
+# 1.2)-----------------------------------------------------------
+def divideATodos11(s: "list[int]", e: int)->bool:
+    res: bool = True
+    for i in range(0 ,len(s)) :  ## Como star=0 es un valor por default, no hace falta escribirlo, in range(len(s)) 
+        if (s[i] % e == 0 ) :
+            res = res and True 
+        else : 
+            res = False
+    return res 
+
+def divideATodos12(s: "list[int]", e: int)->bool:
+    res: bool = True
+    for i in range(len(s)) :
+        if (s[i] % e != 0 ) :
+            res = False
+    return res 
+
+def divideATodos21(s:[int], e: int)->bool:
+    res: bool = True
+    for num in s :
+        if num % e == 0 : 
+            res = res and True
+        else : 
+            res = False
+    return res 
+
+def divideATodos22(s: [int], e: int)->bool:
+    res: bool = True
+    for num in s : 
+        if num % e != 0 :
+            res = False 
+    return res 
+
+def divideATodos31(s: "list[int]", e: int)->bool:
+    res: bool = True
+    indice_actual: int = 0 
+    while (indice_actual < len(s) and res == True) :
+        if s[indice_actual] % e == 0 :
+            res = True
+        else : 
+            res = False 
+        indice_actual += 1 
+    return res 
+
+def divideATodos32(s: "list[int]", e:int)->bool:
+    res: bool = True 
+    indice_actual: int = 0
+    while ( indice_actual < len(s) and res==True) : 
+        if s[indice_actual] % e != 0 :
+            res = False 
+        indice_actual += 1 
+    return res 
+
 # 1.3)-----------------------------------------------------------
-def suma_total(s: [int])->int:
+def suma_total1(s: [int])->int:
     total: int = 0
     indice_actual: int = 0
     longitud: int = len(s)
@@ -47,6 +107,26 @@ def suma_total(s: [int])->int:
         valor_actual: int = s[indice_actual]
         total = total + valor_actual
         indice_actual += 1
+    return total 
+
+def suma_total2(s: [int])->int:
+    total:int = 0 
+    indice_actual: int = 0
+    while (indice_actual < len(s) ) :
+        total = total + s[indice_actual]
+        indice_actual += 1
+    return total 
+
+def suma_total3(s: [int])->int : 
+    total: int = 0 
+    for num in s :
+        total = total + num 
+    return total
+
+def suma_total4(s: [int])->int :
+    total: int = 0
+    for i in range(len(s)) :
+        total = total + s[i]
     return total 
 
 # 1.7)-----------------------------------------------------------
@@ -104,6 +184,6 @@ def perteneceACadaUno1(s: [[int]], e: int)->[bool]:
         resFinal.append(resElemento)
     return resFinal              
 
-print(perteneceACadaUno1([[1,2,3],[4,2,6],[1,8,2]],7))
+# print(perteneceACadaUno1([[1,2,3],[4,2,6],[1,8,2]],7))
 
 
